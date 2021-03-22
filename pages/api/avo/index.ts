60543e674f453b9423c59e6d6d6e04402100f6a0
Boris Muñoz <bmunoz89@users.noreply.github.com>
2021-03-21 21:41:13 -0300

    ref: upgrade get all avocados api

--- pages/api/avo/index.ts
+++ pages/api/avo/index.ts
@@ -1,21 +1,14 @@
 import DB from '@database'
-import { IncomingMessage, ServerResponse } from 'node:http'
+import { NextApiHandler } from 'next'
 
-const allAvos = async (
-  _request: IncomingMessage,
-  response: ServerResponse
-): Promise<void> => {
+const allAvos: NextApiHandler = async (_request, response): Promise<void> => {
   const db = new DB()
   const allEntries = await db.getAll()
 
-  response.statusCode = 200
-  response.setHeader('Content-Type', 'application/json')
-  response.end(
-    JSON.stringify({
-      length: allEntries.length,
-      data: allEntries,
-    })
-  )
+  response.status(200).json({
+    length: allEntries.length,
+    data: allEntries,
+  })
 }
 
 export default allAvos
